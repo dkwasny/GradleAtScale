dda33a6544564026314d65b8c3668cb3d9197b4d
dkwasny <dkwasny67@gmail.com>
2016-05-07 01:09:57 -0400

    Fixing issues with build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+DIR=$(dirname $0);
+
+cd "$DIR";
+
+if [ ! -e .gradle ]; then
+	mkdir .gradle;
+fi;
+
 for i in *-bom; do
-	pushd $i;
+	pushd "$i";
 	mvn install;
 	popd;
 done;
 
 for i in fizz-provider buzz-provider fizzbuzz-provider fizzbuzz-service; do
-	pushd $i;
+	pushd "$i";
 		./gradlew clean test install;
 	popd;
 done;
