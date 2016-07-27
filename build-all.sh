3981423fd2d0b2ac3583d1d65ec76c57b349fdc1
David Kwasny <dkwasny67@gmail.com>
2016-07-27 18:56:02 -0400

    Adding support for missing buildscripts to build-all

--- build-all.sh
+++ build-all.sh
@@ -8,14 +8,24 @@ if [ ! -e .gradle ]; then
 	mkdir .gradle;
 fi;
 
-for i in $(ls | grep "bom$"); do
+for i in common-bom provider-bom; do
 	pushd "$i";
-	mvn install;
+	if [ -f "pom.xml" ]; then
+		echo "Building $i...";
+		mvn install;
+	else
+		echo "Skipping $i...";
+	fi;
 	popd;
 done;
 
 for i in fizz-provider buzz-provider fizzbuzz-provider fizzbuzz-service; do
 	pushd "$i";
+	if [ -f "build.gradle" ]; then
+		echo "Building $i...";
 		./gradlew clean test install;
+	else
+		echo "Skipping $i...";
+	fi;
 	popd;
 done;
